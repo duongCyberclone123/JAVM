3e70981148c2e665e0f7abd0d3d9538dd794f6b1
duongCyberclone123 <duong.maibk106khmt@hcmut.edu.vn>
2025-03-11 15:39:37 +0700

    Init project

---
+++ StackFrame.cpp
@@ -0,0 +1,379 @@
+#include "StackFrame.h"
+#include <iostream>
+#include <fstream>
+#include "errors.h"
+#include "constants.h"
+using namespace std;
+
+StackFrame::StackFrame() : opStackMaxSize(OPERAND_STACK_MAX_SIZE), localVarSpaceSize(LOCAL_VARIABLE_SPACE_SIZE) {}
+
+
+void StackFrame::ArrayAVL::levelTraverse(Node*root, int i){
+    if (!root) {
+        return;
+    }
+    else if (i==1){
+        cout<<"["<<root->key<<": "<<root->getVal()<<", "<<root->getState()<<"]\t";
+    }
+    else{
+        levelTraverse(root->left,i-1);
+        levelTraverse(root->right,i-1);
+    }
+}
+
+void StackFrame::ArrayAVL::printNode(Node* root){
+    if (!root) {
+        cout<<"Empty Tree!\n";
+        return;
+    }
+    int h = getHeightRec(root);
+    cout<<h<<"\n";
+    for (int i=1;i<=h;i++){
+        levelTraverse(root,i);
+        cout<<"\n";
+    }
+}
+
+bool isInt(int a, int b){
+    return (a==0&&b==0);
+}
+bool isFloat(int a,int b){
+    return (a==1 && b==1);
+}
+bool isInt(int a){
+    return !a;
+}
+
+void StackFrame::ArrayAVL::printTree(Node *node, std::string prefix, bool isLeft) {
+    if (node != nullptr) {
+        cout << prefix;
+        cout << (isLeft ? "|---" : "'---");
+
+        // Print the value of the node
+        cout << node->key;
+        if (node->state == 0) {
+            cout << " " << node->val << " (int)" << endl;
+        } else if (node->state == 1) {
+            cout << " " << node->val << " (float)" << endl;
+        }
+
+        // Enter the next tree level - left and right branch
+        printTree(node->left, prefix + (isLeft ? "|\t" : "\t"), true);
+        printTree(node->right, prefix + (isLeft ? "|\t" : "\t"), false);
+    }
+}
+
+
+void splitString(string st, string &com, double &num, string& key){
+    for (int i=st.size()-1;i>=0;i--){
+        if (st[i]==' ') st.pop_back();
+        else break;
+    }
+    size_t sp = st.find(' ');
+    if (sp != string::npos){
+        com=st.substr(0,sp);
+        if (com != "istore" && com != "val" && com != "fload" && com != "iload" && com!="fstore" && com!= "par") num = stod(st.substr(sp+1,st.size()));
+        else key = st.substr(sp+1,st.size());
+    }
+    else {com=st;}
+}    
+
+void StackFrame::run(string filename) {
+    stack<Stack*> stackFrame;
+    ArrayAVL* ArrayTree = new ArrayAVL();
+    
+    fstream file(filename,ios::in);
+    string line;
+    int num_line=0;
+    int size=0;
+    if (!file.is_open()){
+        cout<<"Not open!";
+        return;
+    }
+    
+    while (!file.eof()){
+        getline(file,line);
+        double num=0.0;
+        string com="";
+        string key="";
+        splitString(line,com,num,key);
+        num_line++;
+        //cout<<"Command line no."<<num_line<<" with command \""<<com<<"\""<<"\n";
+        if (com=="iconst"){
+            if (size >= opStackMaxSize) throw StackFull(num_line);
+            stackFrame.push(new Stack(int(num),0));
+        }
+        else if (com=="fconst"){
+            if (size >= opStackMaxSize) throw StackFull(num_line);
+            stackFrame.push(new Stack(num,1));
+        }
+        else if (com=="iadd"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            stackFrame.push(new Stack(int(a->getVal())+int(b->getVal()),0));
+        }
+        else if (com=="fadd"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            stackFrame.push(new Stack(a->getVal()+b->getVal(),1));
+        }
+        else if (com=="isub"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            stackFrame.push(new Stack(int(b->getVal()) - int(a->getVal()),0));
+        }
+        else if (com=="fsub"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            stackFrame.push(new Stack(b->getVal() - a->getVal(),1));
+        }
+        else if(com=="idiv"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            if (a->getVal() == 0.0) throw DivideByZero(num_line); 
+            stackFrame.push(new Stack(int(int(b->getVal()) / int(a->getVal())),0));
+        }
+        else if(com=="fdiv"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            //if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            if (a->getVal() == 0.0) throw DivideByZero(num_line); 
+            stackFrame.push(new Stack(b->getVal() / a->getVal(),1));
+        }
+        else if (com=="irem"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            if (a->getVal() == 0.0) throw DivideByZero(num_line);
+            stackFrame.push(new Stack(int(b->getVal()) - int(int(b->getVal()) / int(a->getVal())*int(a->getVal())),0));
+        }
+        else if (com=="ineg"){
+            if (size<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            if(!isInt(a->getState())) throw TypeMisMatch(num_line);
+            if (!a->getState()){
+                stackFrame.push(new Stack(-int(a->getVal()),0));
+            }
+        }
+        else if (com=="fneg"){
+            if (size<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            stackFrame.push(new Stack(-double(a->getVal()),1));
+        }
+        else if (com=="iand"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            stackFrame.push(new Stack(int(a->getVal()) & int(b->getVal()),0));
+        }
+        else if (com=="ior"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            stackFrame.push(new Stack(int(a->getVal()) | int(b->getVal()),0));
+        }
+        else if (com=="ieq"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            stackFrame.push(new Stack(int(a->getVal()) == int(b->getVal()),0));
+        }
+        else if (com=="feq"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            //if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            stackFrame.push(new Stack((a->getVal()) == (b->getVal()),0));
+        }
+        else if (com=="ineq"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            stackFrame.push(new Stack(int(a->getVal()) != int(b->getVal()),0));
+        }
+        else if (com=="fneq"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            //if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            stackFrame.push(new Stack((a->getVal()) != (b->getVal()),0));
+        }
+        else if (com=="ilt"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            stackFrame.push(new Stack(int(a->getVal()) > int(b->getVal()),0));
+        }
+        else if (com=="flt"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            //if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            stackFrame.push(new Stack((a->getVal()) > (b->getVal()),0));
+        }
+        else if (com=="igt"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            stackFrame.push(new Stack(int(a->getVal()) < int(b->getVal()),0));
+        }
+        else if (com=="fgt"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            //if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            stackFrame.push(new Stack((a->getVal()) < (b->getVal()),0));
+        }
+        else if (com=="ibnot"){
+            if (size<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            if (!a->getState()){
+                int b = a->getVal(); 
+                stackFrame.push(new Stack(b==0,0));
+            }
+            else throw TypeMisMatch(num_line);
+        }
+        else if (com=="imul"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            if (!(isInt(a->getState(), b->getState()))) throw TypeMisMatch(num_line);
+            stackFrame.push(new Stack(int(a->getVal())*int(b->getVal()),0));
+        }
+        else if (com=="fmul"){
+            if (size-1<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            Stack* b = stackFrame.top();
+            stackFrame.pop();
+            stackFrame.push(new Stack(a->getVal()*b->getVal(),1));
+        }
+        else if (com == "istore"){
+            //cout<<ArrayTree->getNum()<<" \n";
+            if (ArrayTree->getNum()*2 >= localVarSpaceSize) throw LocalSpaceFull(num_line);
+            if (stackFrame.empty()) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            if (a->getState()) throw TypeMisMatch(num_line);
+            ArrayTree->insert(a->getVal(),0,key);
+        }
+        else if (com == "fstore"){
+            if (ArrayTree->getNum()*2 >= localVarSpaceSize) throw LocalSpaceFull(num_line);
+            if (stackFrame.empty()) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            if (!a->getState()) throw TypeMisMatch(num_line);
+            ArrayTree->insert(a->getVal(),1,key);
+        }
+        else if (com == "iload"){
+            if (ArrayTree->getNum()==0 || !ArrayTree->searchNode(key)){
+                throw UndefinedVariable(num_line);
+            }
+            StackFrame::ArrayAVL::Node* temp = ArrayTree->searchNode(key);
+            if (temp->getState()) throw TypeMisMatch(num_line);
+            if (size >= opStackMaxSize) throw StackFull(num_line);
+            stackFrame.push(new Stack(temp->getVal(),temp->getState()));
+        }
+        else if (com == "fload"){
+            if (ArrayTree->getNum()==0 || !ArrayTree->searchNode(key)){
+                throw UndefinedVariable(num_line);
+            }
+            StackFrame::ArrayAVL::Node* temp = ArrayTree->searchNode(key);
+            if (!temp->getState()) throw TypeMisMatch(num_line);
+            if (size >= opStackMaxSize) throw StackFull(num_line);
+            stackFrame.push(new Stack(temp->getVal(),temp->getState()));
+        }
+        else if (com=="i2f"){
+            if (size<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            if (!a->getState()){
+                int b = a->getVal(); 
+                stackFrame.push(new Stack(double(b),1));
+            }
+            else throw TypeMisMatch(num_line);
+        }
+        else if (com=="f2i"){
+            if (size<=0) throw StackEmpty(num_line);
+            Stack* a = stackFrame.top();
+            stackFrame.pop();
+            if (a->getState()){
+                double b = a->getVal(); 
+                stackFrame.push(new Stack(int(b),0));
+            }
+            else throw TypeMisMatch(num_line);
+        }
+        else if (com=="val"){
+            if (ArrayTree->getNum()==0 || !ArrayTree->searchNode(key)){
+                throw UndefinedVariable(num_line);
+            }
+            cout<<ArrayTree->searchNode(key)->getVal()<<"\n";
+        }
+        else if (com=="top"){
+            if (size<=0) throw StackEmpty(num_line);
+            cout<<stackFrame.top()->getVal()<<"\n";
+        }
+        else if (com == "par"){
+            if (!ArrayTree->parentNode(ArrayTree->getRoot(), key)) cout<<"null\n";
+            else cout<<ArrayTree->parentNode(ArrayTree->getRoot(), key)->getKey()<<"\n";
+        }
+        size = stackFrame.size()*2;
+    }
+    //ArrayTree->printTree(ArrayTree->getRoot(),"",false);
+    file.close();
+}
---
+++ StackFrame.h
@@ -0,0 +1,242 @@
+#ifndef __STACK_FRAME_H__
+#define __STACK_FRAME_H__
+
+#include <string>
+#include <stack>
+
+
+
+/*
+StackFrame declaration
+*/
+enum{
+    S, E, G
+};
+class StackFrame {
+    int opStackMaxSize; // max size of operand stack
+    int localVarSpaceSize; // size of local variable array
+public:
+    /*
+    Constructor of StackFrame
+    */
+    StackFrame();
+    class Stack{
+        private:
+            double val;
+            int state;
+        public:
+            Stack(double val=0.0,int state=0){
+                this->val=val;
+                this->state = state;
+            }
+            double getVal(){
+                return val;
+            }   
+            int getState(){
+                return state;
+            }
+            void setVal(double val){
+                this->val=val;
+            } 
+            void setState(int state){
+                this->state=state;
+            }
+            ~Stack(){}
+    };
+    class ArrayAVL{
+        public: class Node;
+        private:
+            Node*root;
+            int num_node;
+        public:
+            class Node{
+                private:        
+                    double val;
+                    int state;
+                    std::string key;
+                    friend class ArrayAVL;
+                public:
+                Node* left;
+                Node*right;
+                
+                Node(double val = 0.0,int state = -1, std::string key = ""){
+                    left=nullptr;
+                    right=nullptr;
+                    this->val = val;
+                    this->state = state;
+                    this->key = key;
+                }
+
+                double getVal(){
+                return val;
+                }   
+                int getState(){
+                    return state;
+                }
+                void setVal(double val){
+                    this->val=val;
+                } 
+                void setState(int state){
+                    this->state=state;
+                }
+                std::string getKey(){return key;}
+            };
+                ArrayAVL(){
+                    root=NULL;
+                    num_node=0;
+                }
+                //fix
+                int getHeightRec(Node *node)
+                {
+                    if (node == NULL)
+                        return 0;
+                    int lh = this->getHeightRec(node->left);
+                    int rh = this->getHeightRec(node->right);
+                    return (lh > rh ? lh : rh) + 1;
+                }
+                int getHeight()
+                {
+                    return this->getHeightRec(this->root);
+                }
+
+                int getBalance(Node* root) {
+                    if (!root) return 0;
+                    return (getHeightRec(root->left)-getHeightRec(root->right));
+                }
+
+                // 1st str to be smaller
+                int strcmp(std::string a, std::string b){
+                    int n = a.size() > b.size()?b.size():a.size();
+                    for (int i = 0; i < n; i++){
+                        if (a[i] < b[i]) return S;
+                        else if (a[i] > b[i]) return G;
+                    }
+                    if (a.size() > b.size()) return G;
+                    else if (a.size() == b.size()) return E;
+                    else return S;
+                }
+
+                Node* rotateRight(Node* root) {
+                //TODO: Rotate and return new root after rotate
+                    Node* temp=root->left;
+                    root->left=temp->right;
+                    temp->right=root;
+                    return temp;
+                }
+
+                Node* rotateLeft(Node* root) {
+                //TODO: Rotate and return new root after rotate
+                    Node*temp=root->right;
+                    root->right=temp->left;
+                    temp->left=root;
+                    return temp;
+                }
+
+                Node* insertNode(Node*root,double val, int state, std::string key) {
+                    if (!root) {
+                        return new Node(val, state, key);
+                    }
+                
+                    if (strcmp(key,root->key) == S) {
+                        root->left= insertNode(root->left, val, state, key);
+                    } else {
+                        root->right= insertNode(root->right, val,state, key);
+                    } 
+                   
+                    int balance=getBalance(root);
+
+                    if (balance > 1 && strcmp(key, root->left->key) == S) {
+                        return rotateRight(root);
+                    }
+                    //right right case
+                    if (balance<-1 && (strcmp(key, root->right->key) == G || strcmp(key, root->right->key) == E)) {
+                        return rotateLeft(root);
+                    }
+                    //left right case
+                    if (balance>1 && (strcmp(key,root->left->key) == G || strcmp(key,root->left->key) == E)) {
+                        root->left=rotateLeft(root->left);
+                        return rotateRight(root);
+                    }
+                    if (balance<-1 && strcmp(key, root->right->key) == S) {
+                        root->right=rotateRight(root->right);
+                        return rotateLeft(root);
+                    }
+                    return root;
+                }
+
+                void insert(double val, int state, std::string key){
+                    //TODO
+                    if (!root) {
+                        num_node++;
+                        
+                        root=new Node(val, state, key);
+
+                    }
+                    Node* existNode = searchNode(key);
+                    if (existNode){
+                        existNode->setState(state);
+                        existNode->setVal(val);
+                        return;
+                    }
+                    else {
+                        root=insertNode(root,val, state, key);
+                        num_node++;
+                    }
+                }
+
+                //end fix
+                int getNum(){return num_node;}
+
+                Node* getRoot(){return root;}
+
+                void setRoot(Node*node){root = node;}
+
+                Node* levelTraverse(Node* root, int level, std::string key) {
+                    if (!root) return NULL;
+                    if (level == 1) {
+                        if (root->key == key) return root;
+                    } else {
+                        Node* leftResult = levelTraverse(root->left, level - 1, key);
+                        if (leftResult) return leftResult;  
+                        return levelTraverse(root->right, level - 1, key);  
+                    }
+                    return NULL;
+                }
+
+                Node* parentNode(Node* root, std::string key){
+                    if (!root) return NULL;
+                    if (!root->left && !root->right) return NULL;
+                    else if ((root->left && root->left->getKey() == key) || (root->right && root->right->getKey() == key)) return root;
+                    else if (!root->left && root->right) return parentNode(root->right, key);
+                    else if (root->left && !root->right) return parentNode(root->left, key) ;
+                    else {
+                        int comp = strcmp(root->getKey(), key);
+                        if (comp == G) return parentNode(root->left, key);
+                        else return parentNode(root->right, key);
+                    }
+                }
+
+                Node* searchNode(std::string key) {
+                    int h = getHeight();
+                    for (int i = 1; i <= h; i++) {
+                        Node* temp = levelTraverse(root, i, key);
+                        if (temp) {
+                            return temp;
+                        }  // Return immediately if node is found
+                    }
+                    return NULL;  // Return NULL if node is not found
+                }
+
+                void levelTraverse(Node*root, int i);
+
+                void printNode(Node*root);
+                void printTree(Node *node, std::string prefix, bool isLeft);
+    };
+    /*
+    Run the method written in the testcase
+    @param filename name of the file
+    */
+    void run(std::string filename);
+};
+
+#endif // !__STACK_FRAME_H__
